3f4edf96c6321543747ae3a33b7336a606600d61
Paul Thomas <paulthomas2107@gmail.com>
2023-11-06 20:24:00 +0000

    More

--- main.py
+++ main.py
@@ -12,15 +12,54 @@ BACKGROUND_COLOR = "#000000"
 
 
 class Snake:
-    pass
+    def __init__(self):
+        self.body_size = BODY_PARTS
+        self.coordinates = []
+        self.squares = []
+
+        for i in range(0, BODY_PARTS):
+            self.coordinates.append([0, 0])
+
+        for x, y in self.coordinates:
+            square = canvas.create_rectangle(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=SNAKE_COLOR, tags="snake")
+            self.squares.append(square)
 
 
 class Food:
-    pass
+    def __init__(self):
+        x = random.randint(0, int((GAME_WIDTH / SPACE_SIZE) - 1)) * SPACE_SIZE
+        y = random.randint(0, int((GAME_HEIGHT / SPACE_SIZE) - 1)) * SPACE_SIZE
+
+        self.coordinates = [x, y]
+
+        canvas.create_oval(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=FOOD_COLOR, tags="food")
 
 
-def next_turn():
-    pass
+def next_turn(snake, food):
+    x, y = snake.coordinates[0]
+
+    if direction == 'up':
+        y -= SPACE_SIZE
+    elif direction == 'down':
+        y += SPACE_SIZE
+    elif direction == 'left':
+        x -= SPACE_SIZE
+    elif direction == 'right':
+        x += SPACE_SIZE
+
+    snake.coordinates.insert(0, (x, y))
+
+    square = canvas.create_rectangle(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=SNAKE_COLOR)
+
+    snake.squares.insert(0, square)
+
+    del snake.coordinates[-1]
+
+    canvas.delete(snake.squares[-1])
+
+    del snake.squares[-1]
+
+    window.after(SPEED, next_turn, snake, food)
 
 
 def change_direction(new_direction):
@@ -59,5 +98,10 @@ x = int((screen_width / 2) - (window_width / 2))
 y = int((screen_height / 2) - (window_height / 2))
 
 window.geometry(f"{window_width}x{window_height}+{x}+{y}")
+
+snake = Snake()
+food = Food()
+
+next_turn(snake, food)
 
 window.mainloop()
